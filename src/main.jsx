9942c6d66969ac1ddc030bf105427f33ffd72b90
jmaitner <jackson.maitner@example.com>
2025-08-18 22:57:22 -0400

    task-02: scaffold all required routes with React Router and placeholder components

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,38 @@
 ﻿import 'flowbite/dist/flowbite.css';
 import './index.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function Home() {
+  return <div>Home Page</div>;
+}
+function Find() {
+  return <div>Find Results Page</div>;
+}
+function Course() {
+  return <div>Course Page</div>;
+}
+function Support() {
+  return <div>Support Page</div>;
+}
+function Privacy() {
+  return <div>Privacy Policy Page</div>;
+}
+function Terms() {
+  return <div>Terms of Service Page</div>;
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/find/:state/:courseType" element={<Find />} />
+        <Route path="/courses/:slug" element={<Course />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
